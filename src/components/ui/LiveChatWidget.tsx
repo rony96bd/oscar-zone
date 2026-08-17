b1b2b5423659664f225a847a3ddf20e7c1f51c07
Md. Rakib Uddin <r.rony9966@gmail.com>
2026-08-17 13:17:02 +0600

    fix: increase LiveChatWidget z-index

--- src/components/ui/LiveChatWidget.tsx
+++ src/components/ui/LiveChatWidget.tsx
@@ -117,7 +117,7 @@ export function LiveChatWidget() {
   }
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-6 right-6 z-[9999]">
       {/* Floating Button */}
       {!isOpen && (
         <button
