8d6dde3bb9a9a1b46ac0e119a57405ced5d10e1d
Md. Rakib Uddin <r.rony9966@gmail.com>
2026-09-03 09:41:23 +0600

    Update HomePage popular games section to use 2-column mobile grid

--- src/pages/public/HomePage.tsx
+++ src/pages/public/HomePage.tsx
@@ -189,7 +189,7 @@ export default function HomePage() {
             </Link>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             {games.map((game) => (
               <GameCard
                 key={game.id}
