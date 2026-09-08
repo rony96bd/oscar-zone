b67560e8e361faf5dbc6165b5ea9b5769bd5e916
Md. Rakib Uddin <r.rony9966@gmail.com>
2026-09-08 17:53:21 +0600

    style: increase top padding for main content to prevent ticker overlap

--- src/components/layout/CustomerLayout.tsx
+++ src/components/layout/CustomerLayout.tsx
@@ -307,7 +307,7 @@ export function CustomerLayout() {
       </div>
 
       {/* Main Content Area */}
-      <main className={cn("flex-1 relative z-10", tickerPosition === 'header' ? 'pt-24' : 'pt-16')}>
+      <main className={cn("flex-1 relative z-10", tickerPosition === 'header' ? 'pt-32' : 'pt-16')}>
         <Outlet />
       </main>
       
